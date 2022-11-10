961b5e78fca4ed6032bc41871935b938f0555af7
Miłosz Pawłowski <milosz.pawlowski.torun@wp.pl>
2022-11-10 20:36:56 +0100

    Add files via upload

--- public/js/dom-elements.js
+++ public/js/dom-elements.js
@@ -2,8 +2,8 @@ const createCountryElement = (country) => {
     const countryElement = document.createElement("li");
     countryElement.classList.add("card");
     countryElement.innerHTML = `
-        <h2 class="text-2xl text-center">${country.name}<h2>
-        <img src=${country.flag}>
+        <h2 class="title">${country.name}</h2>
+        <img class="photo" src=${country.flag}>
         <h3>Region: ${country.region}</h3>
         <p>Capital: ${country.capital}</p>
         <p>Population: ${country.population}</p>
@@ -14,6 +14,7 @@ const createCountryElement = (country) => {
 
 const createListElement = (countries) => {
     const listElement = document.createElement("ul");
+    listElement.classList.add("list");
     countries.forEach(country => {
         listElement.appendChild(createCountryElement(country));
     });
